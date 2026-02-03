f5eebdd90fb9149c99017aab25b3c8253c09ecea
JoJoe2004 <joeza21195@gmail.com>
2026-02-03 16:43:41 +0700

    use sendgrid for otp

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -5,8 +5,8 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 export const sendOtpEmail = async (to: string, otp: string) => {
   await sgMail.send({
     to,
-    from: process.env.EMAIL_USER!, // ต้องเป็น email ที่ยืนยันใน SendGrid
+    from: process.env.SENDGRID_FROM_EMAIL!, // ต้องตรงกับ Sender
     subject: "รหัส OTP ของคุณ",
-    text: `รหัส OTP ของคุณคือ ${otp} (หมดอายุใน 5 นาที)`
+    text: `รหัส OTP ของคุณคือ ${otp}`,
   });
 };
